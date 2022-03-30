0937e17a1cf5c837630ffb160871bc50947d460e
xiao hui <80144167+mouxiaohui@users.noreply.github.com>
2022-03-30 15:00:20 +0800

    获取视频下载地址

--- core/down.go
+++ core/down.go
@@ -4,17 +4,18 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/mouxiaohui/bili-go/cmd"
 )
 
 var (
-	VIEW_URL     string       = "https://api.bilibili.com/x/web-interface/view"
-	PASSPORT_URL string       = "https://passport.bilibili.com"
-	CLIENT       *http.Client = &http.Client{Timeout: time.Duration(10) * time.Second}
+	BASE_URL string       = "https://api.bilibili.com/"
+	CLIENT   *http.Client = &http.Client{Timeout: time.Duration(10) * time.Second}
 )
 
 func Run() error {
@@ -28,29 +29,92 @@ func Run() error {
 		return errors.New("未找到视频❗")
 	}
 
+	err = downloadVideo(&videoInfo)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
+// 获取视频信息
 func getVideoInfo(bvid string) (VideoInfo, error) {
 	var resultInfo ResultInfo[VideoInfo]
-	url := fmt.Sprintf("%s?bvid=%s", VIEW_URL, bvid)
+	url := fmt.Sprintf("%s%s?bvid=%s", BASE_URL, "x/web-interface/view", bvid)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return resultInfo.Data, err
 	}
-	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := CLIENT.Do(req)
 	if err != nil {
 		return resultInfo.Data, err
 	}
-	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	bodyString, err := readCloserToString(&resp.Body)
 	if err != nil {
 		return resultInfo.Data, err
 	}
-	bodyString := string(body)
 
 	err = json.Unmarshal([]byte(bodyString), &resultInfo)
-	return resultInfo.Data, err
+	if err != nil {
+		return resultInfo.Data, err
+	}
+
+	return resultInfo.Data, nil
+}
+
+// 获取视频下载连接地址等信息
+func getVideoUrl(url string) (VideoUrl, error) {
+	var resultInfo ResultInfo[VideoUrl]
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return resultInfo.Data, err
+	}
+
+	resp, err := CLIENT.Do(req)
+	if err != nil {
+		return resultInfo.Data, err
+	}
+	defer resp.Body.Close()
+
+	bodyString, err := readCloserToString(&resp.Body)
+	if err != nil {
+		return resultInfo.Data, err
+	}
+
+	err = json.Unmarshal([]byte(bodyString), &resultInfo)
+	if err != nil {
+		return resultInfo.Data, err
+	}
+
+	return resultInfo.Data, nil
+}
+
+// 下载视频
+func downloadVideo(videoInfo *VideoInfo) error {
+	videoUrl, err := getVideoUrl(
+		fmt.Sprintf(
+			"%s%s?fnval=80&avid=%s&cid=%s",
+			BASE_URL,
+			"x/player/playurl",
+			strconv.Itoa(videoInfo.Aid),
+			strconv.Itoa(videoInfo.Cid),
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readCloserToString(rc *io.ReadCloser) (string, error) {
+	body, err := ioutil.ReadAll(*rc)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
--- core/models.go
+++ core/models.go
@@ -13,4 +13,55 @@ type VideoInfo struct {
 	Title string `json:"title"`
 }
 
-type VideoUrl struct{}
+type VideoUrl struct {
+	Dash Dash `json:"dash"`
+}
+
+type Dash struct {
+	Duration int     `json:"duration"`
+	Videos   []Video `json:"video"`
+	Audios   []Audio `json:"audio"`
+}
+
+type Video struct {
+	Id        int    `json:"id"`
+	BaseUrl   string `json:"baseUrl"`
+	BandWidth int    `json:"bandwidth"`
+	Width     int    `json:"width"`
+	Height    int    `json:"height"`
+}
+
+type Audio struct {
+	Id        int    `json:"id"`
+	BaseUrl   string `json:"baseUrl"`
+	BandWidth int    `json:"bandwidth"`
+}
+
+// 获取视频质量的文字描述
+func GetQualityById(id int) string {
+	// 一般80以上的都要大会员，无法获取
+	switch id {
+	case 6:
+		return "240P 极速"
+	case 16:
+		return "360P 流畅"
+	case 32:
+		return "480P 清晰"
+	case 64:
+		return "720P 高清"
+	case 74:
+		return "720P60 高帧率"
+	case 80:
+		return "1080P 高清"
+	case 112:
+		return "1080P+ 高码率"
+	case 116:
+		return "1080P60 高帧率"
+	case 120:
+		return "4k 超清"
+	case 125:
+		return "HDR 真彩色"
+	default:
+		return "UNKNOW"
+	}
+}
